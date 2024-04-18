59f8a3b427cfbaa10aedd7648d4a2b8657982a11
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2024-04-19 03:47:47 +0900

    ボイスチャンネルの入室・退出機能を追加します

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@ from discord import app_commands
 import discord
 from ui import *
 
+
 def setup(bot):
     
     bot.remove_command('help')
@@ -25,9 +26,27 @@ def setup(bot):
     @bot.command(description="ボタンを表示します")
     async def button(ctx):
         button = Button()
-        await ctx.send('ボタンを表示します', view=button)
+        await ctx.send('', view=button ,silent=True)  
     
     @bot.command(description="セレクトメニューを表示します")
     async def select(ctx):
         select = Select()
-        await ctx.send('セレクトメニューを表示します', view=select)
+        await ctx.send('', view=select ,silent=True)
+
+    @bot.command(description="ボイスチャンネルに入室します")
+    async def join(ctx):
+        if ctx.author.voice is None:
+            await ctx.send("ボイスチャンネルに参加してから再度試してください。")
+        else:
+            channel = ctx.author.voice.channel
+            if ctx.voice_client is not None:
+                await ctx.send("既にボイスチャンネルに入室しています。")
+            else:
+                await channel.connect()
+
+    @bot.command(description="ボイスチャンネルから退出します")
+    async def leave(ctx):
+        if ctx.voice_client is None:
+            await ctx.send("ボイスチャンネルに参加していません。")
+        else:
+            await ctx.voice_client.disconnect()
