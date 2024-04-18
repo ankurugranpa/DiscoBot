d7ac9947a16951a18143ca52a8a019f553a1abdb
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2024-04-19 01:27:13 +0900

    ボタンを追加してメッセージを送信する。

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+class Button(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        button = discord.ui.Button(label='Press Me', style=discord.ButtonStyle.primary, custom_id='button:press')
+        button.callback = self.on_button_press  # ボタンのコールバックを設定
+        self.add_item(button)
+
+    async def on_button_press(self, interaction):
+        user = interaction.user
+        await interaction.response.send_message(f"{user}さんがボタンを押しました",silent=True)  # ボタンが押されたときの応答
+
+
 
 @client.event
 async def on_ready():
@@ -23,6 +35,9 @@ async def on_message(message):
         return
 
     if message.content.startswith('hello'):
-        await message.channel.send('Helloo!',silent=True)
+        view = Button()
+        await message.channel.send('Hello!',silent=True, view=view)
+    
+
 
 client.run(TOKEN)
